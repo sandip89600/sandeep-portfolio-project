253aff02febfb6dc8b42f5ac65db78e61a9b571e
Sandeep Pandit <sandippandit896@gmail.com>
2026-02-23 17:51:20 +0530

    update mobile

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,15 +4,30 @@ import UserLeft from '../Components/User/UserLeft'
 
 const Home = () => {
   return (
-    <div className='flex'>
-      <div className="w-1/2 p-20">
-      <UserLeft/>
-         </div>
-      <div className="w-1/2 h-138">
- 
- 
-        <img className='object-cover  hover:scale-105 duration-400  w-90 rounded-full ml-40 mt-20 ring-8 ring-orange-500 shadow-[0_0_40px_rgba(255,138,0,0.35)] ' src={images.hero} alt="" />
+    <div className='flex flex-col md:flex-row items-center justify-between min-h-screen px-6 md:px-16 lg:px-24'>
+
+      {/* LEFT SECTION */}
+      <div className="w-full md:w-1/2 text-center md:text-left mt-10 md:mt-0">
+        <UserLeft />
       </div>
+
+      {/* RIGHT SECTION */}
+      <div className="w-full md:w-1/2 flex justify-center mt-10 md:mt-0">
+        <img
+          className='
+            w-60 sm:w-72 md:w-80 lg:w-96
+            object-cover
+            rounded-full
+            transition-transform duration-500
+            hover:scale-105
+            ring-4 md:ring-8 ring-orange-500
+            shadow-[0_0_40px_rgba(255,138,0,0.35)]
+          '
+          src={images.hero}
+          alt="Hero"
+        />
+      </div>
+
     </div>
   )
 }
